47c495ae384afb62e9c0722269d55ffbb6875e04
Alex <Yulien.ru@Yandex.ru>
2022-09-03 21:20:48 +0400

    добавлено решение первой задачи из домашки

---
+++ HomeTask3/Program.cs
@@ -0,0 +1,27 @@
+﻿// Разбор домашнего задания с третьего семинара
+// Task19
+// bool Palindrome (int num){
+//     int n1 = num / 10000;
+//     num -= n1 * 10000;
+
+//     int n2 = num / 1000;
+//     num -= n2 * 1000;
+    
+//     int n3 = num / 100;
+//     num -= n3 * 100;
+
+//     int n4 = num / 10;
+//     int n5 = num % 10;
+
+//     if (n1 ==n5 && n2 == n4){
+//         return true;
+//     }
+//     else 
+//         return false;
+    
+// }
+// System.Console.WriteLine(Palindrome(12322));
+
+// Task21
+
+// Task23
