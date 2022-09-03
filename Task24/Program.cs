c65fc999a36be4907a987cf634b8c04845853305
Alex <Yulien.ru@Yandex.ru>
2022-09-03 21:41:26 +0400

    сумма чисел от 1 до А

---
+++ Task24/Program.cs
@@ -0,0 +1,33 @@
+﻿// Напишите программу, которая принимает на вход число (А) и выдает сумму чисел от 1 до А.
+// 7-=28
+// 4-=10
+// 8-=36
+
+// 1 вариант
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// int GetSum (int n){
+//     int sum = 0;
+
+//     int i = 1;
+//     while (i <=n){
+//         sum += i;
+//         i++;
+//     }
+//     return sum;
+// }
+// System.Console.WriteLine(GetSum(num));
+
+//2 вариант
+System.Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int GetSum (int n){
+    int sum = 0;
+
+    for (int i = 1; i <=n; i++){
+        sum += i;
+    }
+    return sum;
+}
+System.Console.WriteLine(GetSum(num));
