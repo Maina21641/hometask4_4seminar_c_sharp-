7fa76e8cd91899eeede951b36dba2919294ef354
Alex <Yulien.ru@Yandex.ru>
2022-09-03 23:43:01 +0400

    массив из нулей и елиниц

--- TaskMassiv10101010/Program.cs
+++ TaskMassiv10101010/Program.cs
@@ -1,28 +1,47 @@
 ﻿//Напишите программу, которая выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке.
 //[1,0,1,1,0,1,0,0]
+//сокращенный вариант из лекции
+// void FillArray(int[] collection) // в качестве аргумента какая-то коллекция
+// {
+//     int length = collection.Length;
+//     int index = 0;
+//     while (index < length)
+//     {
+//         collection[index] = new Random().Next (0, 2); // обращаемся к аргументу collection на позицию Index и положить туда новое случайное целое число из диапазона 0-1
+//         index++;
+//     }
+// }
+// void PrintArray(int[] col)
+// {
+//     int count = col.Length;
+//     int position = 0;
+//     while (position < count)
+//     {
+//         Console.WriteLine(col[position]);
+//         position++;
+//     }
+//  }
 
-void FillArray(int[] collection) // в качестве аргумента какая-то коллекция
+// int[] array = new int[8]; //создай новый массив в котором будет 8 элементов
+
+// FillArray(array); // метод заполнения массива числами
+// PrintArray(array); // метод печати массива на экран/консоль
+
+//вариант ребят
+
+int[] RandomArray()
 {
-    int length = collection.Length;
-    int index = 0;
-    while (index < length)
+    int[] array = new int[8];
+    for (int i = 0; i < 8; i ++)
     {
-        collection[index] = new Random().Next (0, 2); // обращаемся к аргументу collection на позицию Index и положить туда новое случайное целое число из диапазона 0-1
-        index++;
+        array[i] = new Random().Next(0, 2);
     }
+    return array;
 }
-void PrintArray(int[] col)
+
+int[] arr = RandomArray();
+
+foreach(var item in arr)
 {
-    int count = col.Length;
-    int position = 0;
-    while (position < count)
-    {
-        Console.WriteLine(col[position]);
-        position++;
-    }
- }
-
-int[] array = new int[8]; //создай новый массив в котором будет 8 элементов
-
-FillArray(array); // метод заполнения массива числами
-PrintArray(array); // метод печати массива на экран/консоль
+    System.Console.Write(item + ", ");
+}
